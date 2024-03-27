add5e67806a4c14f2e54c59fe93937eace193ffd
WeimperGergo <weimper.geri@gmail.com>
2024-03-27 15:29:38 +0100

    listagen + gombkezelo init.

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,5 +1,19 @@
 export function lighton() {
     // Kód ide 
-    // const JATEKTER = ;
-    
+    const JATEKTER = $("#jatek");
+
+    function listaGen(meret) {
+        for (let i = 0; i < meret; i++) {
+            // JATEKTER.append( elem )
+            // WG
+        }
+    }
+
+    function gombKezelo(JATEKTER) {
+        // WG
+        for (let i = 0; i < JATEKTER.length; i++) {
+            if(JATEKTER[i] === true) JATEKTER[i] = false;
+            else JATEKTER[i] = true
+        }
+    }
 }
