31bbc04fb69fc2231c98610d81267b30ea76fad2
WeimperGergo <weimper.geri@gmail.com>
2024-04-04 21:54:20 +0200

    kis javitasok

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,46 +1,39 @@
 export function lighton() {
     // Kód ide 
-    const MERET = 3; // Hányszor hányas
+    let meret = 3; // Hányszor hányas
     const JATEKTER = $("#jatekter");
-    const jatekterLISTA = []; 
-    // I/H értékkel teli lista
-    JATEKTER.html(jatekterLetrehozas(MERET));
+    const jatekterLISTA = []; // I/H értékkel teli lista
+    
+    JATEKTER.html(jatekterLetrehozas(meret));   
     const jatekELEMEK = $("#jatekter .gombok"); // Gombok listája
-    listaGen(MERET);
+    listaGen(meret);
     jatekterMegjelenites(jatekterLISTA, jatekELEMEK);
-    jatekterElrendezes(MERET, jatekterLISTA);
+    // jatekVege(jatekterLISTA);
+    vegeFigyelo(jatekterLISTA);
 
-    function jatekterLetrehozas(MERET) {
+    function jatekterLetrehozas(meret) {
         let txt = `<div class="gombok"></div>`;
-        for(let i = 0; i<MERET*MERET-1; i++){
+        for(let i = 0; i<meret*meret-1; i++){
             txt += `<div class="gombok"></div>`;
         }
         return txt;
         // RB
     }
 
-    function jatekterElrendezes(MERET, JATEKTER) {
-        let szoveg = "";
-        for (let i = 0; i < MERET-1; i++) {
-            szoveg += "1fr ";
-        }
-        szoveg += "1fr";
-        JATEKTER.css("grid-template-columns", szoveg);
-    }
 
     function jatekterMegjelenites(lista, jatekELEM){
-        for (let i = 0; i < jatekELEM.length; i++) {
+        for (let i = 0; i < lista.length; i++) {
             if (lista[i] == true) jatekELEM.eq(i).css("background-color", "green");
             else jatekELEM.eq(i).css("background-color", "yellow");
         }
     }
 
-    function listaGen(MERET) {
+    function listaGen(meret) {
         // WG
         /* Megadott hosszig generál minden alkalommal egy I/H értéket 20% eséllyel */
-        for (let i = 0; i < MERET*MERET; i++) {
+        for (let i = 0; i < meret*meret; i++) {
             let rnd = rndGen(0, 100);
-            if (rnd <= 20) jatekterLISTA[i] = true;
+            if (rnd <= 100) jatekterLISTA[i] = true;
             else jatekterLISTA[i] = false;
             // console.log(JATEKTER[i])
         }
@@ -49,10 +42,38 @@ export function lighton() {
     function gombKezelo(LISTA) {
         // WG
         // Rá kell tenni az eseménykezelőt az adott gombra
+        let meret = $(":root");
         for (let i = 0; i < LISTA.length; i++) {
             if(LISTA[i] === true) LISTA[i] = false;
             else LISTA[i] = true;
         }
+        
+    }
+
+    function jatekVege(lista){
+        let vege = false;
+        let talaltLampat = false;
+        let i = 0;
+        while (i < lista.length) {
+            if(lista[i] === true){
+                talaltLampat = true;
+                console.log(i + ". elem");
+            }
+            else if (i == lista.length){
+                vege = true;
+                console.log(i + ". elem");
+            }
+            i++;
+        }
+        if (vege === true) {
+            alert("Hurrá, meghosszabítottad a Föld életét!");
+        }
+    }
+
+    function vegeFigyelo(lista){
+        $(window).on("load", function(){
+            jatekVege(lista)
+        });
     }
 
     function rndGen(min, max) {
